bcc9187153b2d14ca7449d9dfd3c05f5392cbde6
alisson-moura <alisson.mo.moura@outlook.com.br>
2022-11-24 14:56:36 -0300

    fix: corrido schema

--- src/api/web/validation/update-account.ts
+++ src/api/web/validation/update-account.ts
@@ -4,8 +4,8 @@ export const UpdateAccountSchema = Joi.object({
     name: Joi.string().required().min(3),
     email:  Joi.string().email().required(),
     gender: Joi.string().max(1).required(),
-    badge: Joi.number(),
-    photo: Joi.string(),
-    birthDate: Joi.date(),
-    comments: Joi.string()
+    badge: Joi.number().allow(''),
+    photo: Joi.string().allow(''),
+    birthDate: Joi.date().allow(''),
+    comments: Joi.string().allow('')
 })
