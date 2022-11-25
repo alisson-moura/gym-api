acb5634ee2be3f96b543b8d62ed686ed2d4f3bee
alisson-moura <alisson.mo.moura@outlook.com.br>
2022-11-25 15:56:42 -0300

    fix: corrigido texto

--- prisma/seeds/seed.ts
+++ prisma/seeds/seed.ts
@@ -6,7 +6,7 @@ async function main() {
     const term = await prisma.term.create({
         data: {
             title: "Termo de Responsabilidade  para Prática de Atividade Física - Programa Movi Mente-se FUNFARME",
-            text: "Assumo plena responsabilidade por qualquer atividade física praticada e isento a FUNFARME, bem como todos diretamente envolvidos no desenvolvimento das atividades oferecidas e praticadas pro mim, de toda e qualquer responsabilidade por danos a minha saúde ou de qualquer outra espécie, que enventualmente possam ser causados.\n<p>Declaro ainda estar ciente que as aulas são oferecidas de forma gratuita e não integram a duração de minha jornada de trabalho, sendo minha livre e espontânea opção realizá-las."
+            text: "Assumo plena responsabilidade por qualquer atividade física praticada e isento a FUNFARME, bem como todos diretamente envolvidos no desenvolvimento das atividades oferecidas e praticadas pro mim, de toda e qualquer responsabilidade por danos a minha saúde ou de qualquer outra espécie, que enventualmente possam ser causados.\\nDeclaro ainda estar ciente que as aulas são oferecidas de forma gratuita e não integram a duração de minha jornada de trabalho, sendo minha livre e espontânea opção realizá-las."
         }
     })
 
