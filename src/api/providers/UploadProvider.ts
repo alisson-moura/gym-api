c36f36be4bab1bbc50292b3103198002e4ba8836
alisson-moura <alisson.mo.moura@outlook.com.br>
2022-12-01 12:50:12 -0300

    fix: corrigido file size upload

--- src/api/providers/UploadProvider.ts
+++ src/api/providers/UploadProvider.ts
@@ -15,7 +15,7 @@ export const uploadConfig = {
         }
     }),
     limits: {
-        fileSize: 1048576, // 10 Mb
+        fileSize:10485760 // 10 Mb
     },
     fileFilter: (req: Request, file: Express.Multer.File, cb: any) => {
         const allowedMimes = [
